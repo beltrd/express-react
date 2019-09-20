b3ae3e3c72989697d8b329adb255b0a2041db339
diego <beltrd@gmail.com>
2019-09-19 20:14:28 -0500

    added console log when app.listen is called. also added process.env.port

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(__dirname +'./../../')); //serves the index.html
-app.listen(3000); //listens on port 3000 -> http://localhost:3000/
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
